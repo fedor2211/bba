e7a9a0f94b8c82691d9eb9180de1bf0c2c55fc2e
Gwarek2 <fedorsah@gmail.com>
2023-02-07 23:21:18 +0300

    Refactor mailer code

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,7 +7,7 @@ class SubscriptionsController < ApplicationController
     @new_subscription.user = current_user
 
     if @new_subscription.save
-      EventNotificationJob.perform_later(@event, @new_subscription)
+      EventNotificationJob.perform_later(@new_subscription)
       redirect_to event_path(@event), notice: I18n.t("controllers.subscriptions.created")
     else
       render "events/show", alert: I18n.t("controllers.subscriptions.error")
--- app/jobs/event_notification_job.rb
+++ app/jobs/event_notification_job.rb
@@ -1,25 +1,26 @@
 class EventNotificationJob < ApplicationJob
   queue_as :default
 
-  def perform(event, object)
+  def perform(object)
+    @event = object.event
     case object
     when Comment
-      subscribers_emails(event, object).each do |mail|
-        EventMailer.comment(event, object, mail).deliver_later
+      subscribers_emails(object).each do |mail|
+        EventMailer.comment(object, mail).deliver_later
       end
     when Photo
-      subscribers_emails(event, object).each do |mail|
-        EventMailer.photo(event, object, mail).deliver_later
+      subscribers_emails(object).each do |mail|
+        EventMailer.photo(object, mail).deliver_later
       end
     when Subscription
-      EventMailer.subscription(event, object).deliver_later
+      EventMailer.subscription(object).deliver_later
     end
   end
 
   private
 
-  def subscribers_emails(event, object)
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+  def subscribers_emails(object)
+    all_emails = (@event.subscriptions.map(&:user_email) + [@event.user.email]).uniq
     all_emails -= [object.user.email] if object.user.present?
     all_emails
   end
